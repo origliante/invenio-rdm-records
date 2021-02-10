ab4a88d547c7c13db2c355ac06404060db74616e
Tom Morrell <tmorrell@caltech.edu>
2021-02-09 17:01:28 -0800

    Add file writing and updated full_record

--- tests/get_full_record.py
+++ tests/get_full_record.py
@@ -2,5 +2,6 @@ import json
 
 
 def test_get_full_record(full_record):
-    print(full_record)
-
+    fname = 'tests/data/full_record.json'
+    with open(fname, 'w') as outfile:
+        json.dump(full_record, outfile, indent=2)
